8f7863bdd02516714233fdc026896179fb109120
Serena <pizzalover2317@gmail.com>
2024-10-20 19:02:16 -0400

    just the times

--- components/Timer.vue
+++ components/Timer.vue
@@ -30,8 +30,8 @@ const isMobile = computed(() => {
 
 //Timer
 
-const startTime = new Date('2024-10-26T00:00:00'); 
-const endTime = new Date('2024-10-27T12:00:00');
+const startTime = new Date('2024-10-20T00:00:00'); 
+const endTime = new Date('2024-10-21T12:00:00');
 
 const days = ref('00');
 const hours = ref('00');
